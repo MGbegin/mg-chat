b58ad6a9bd042ccba026de49763dc199e6c38160
mayur <mayur.gavande308@gmail.com>
2023-05-25 19:44:08 +0530

    fiest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from './service/message.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mg-chat';
+  message = "";
+  chats = [
+    'hay',
+    'how are you',
+    'i am fine thank you for asking',
+    'well what are you doing right now',
+    'well i am writing a code for chat system',
+    'wow great'
+  ];
+  constructor(message: MessageService){}
+  addChat(){
+    if (this.message.length === 0){
+      return;
+    }
+    this.chats.push(this.message);
+    this.message = '';
+    window.setInterval(() => {
+      const elem = document.getElementById('scrolldiv');
+      if(elem?.scrollTop != null){
+          elem.scrollTop = elem.scrollHeight;
+      }
+      
+    },500);
+  }
 }
